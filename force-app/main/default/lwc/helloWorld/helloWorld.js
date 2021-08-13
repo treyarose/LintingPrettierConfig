9746759cffdb3a7e929d61c84a33c49e50819ccf
trey.arose <trey.arose@mandsconsulting.com>
2021-08-13 13:22:40 -0400

    eslint config

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,7 +1,11 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement, track } from "lwc";
 export default class HelloWorld extends LightningElement {
-    @track greeting = 'World';
-    changeHandler(event) {
-        this.greeting = event.target.value;
-    }
+  @track greeting = "World";
+  changeHandler(event) {
+    this.greeting = event.target.value;
+  }
+
+  handleLint() {
+    console.log("working");
+  }
 }
